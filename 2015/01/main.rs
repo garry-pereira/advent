d5dda345d1ccc70383e17bf026e94b35ebc7d545
Garry Michael Pereira <garrymp12@gmail.com>
2023-10-28 23:39:14 -0500

    nothing

--- 2015/01/main.rs
+++ 2015/01/main.rs
@@ -3,6 +3,7 @@ use std::fs::File;
 fn main() {
     let file = File::open("input.txt");
 
+    // use match to unwrap Result<Ok(file), Err(error)>
     let file = match file {
         Ok(file) => file,
         Err(error) => panic!("Problem opening: {:?}", error),
